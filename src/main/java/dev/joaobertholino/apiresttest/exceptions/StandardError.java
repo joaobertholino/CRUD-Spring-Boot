7a4685df68ef7e7787cddc6cb44804343db60831
João Bertholino <joaobertholino964@gmail.com>
2024-08-03 14:21:36 -0300

    Implementando suporte a serialização e desserialização da classe record StandardError.

--- src/main/java/dev/joaobertholino/apiresttest/exceptions/StandardError.java
+++ src/main/java/dev/joaobertholino/apiresttest/exceptions/StandardError.java
@@ -2,11 +2,16 @@ package dev.joaobertholino.apiresttest.exceptions;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public record StandardError(@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss:SS'Z'", timezone = "GMT") LocalDateTime timestamp,
 														Integer status,
 														String error,
 														String message,
-														String path) {
+														String path) implements Serializable {
+
+	@Serial
+	private static final long serialVersionUID = 1L;
 }
