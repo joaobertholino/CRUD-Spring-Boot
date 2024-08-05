c7042619467d3faae0bd81ee08e11817d3e063bc
João Bertholino <joaobertholino964@gmail.com>
2024-08-04 23:59:40 -0300

    Implementação da camada de Repository de OrderItem e Product.

---
+++ src/main/java/dev/joaobertholino/apiresttest/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package dev.joaobertholino.apiresttest.repositories;
+
+import dev.joaobertholino.apiresttest.models.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
+}
